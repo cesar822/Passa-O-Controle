f4dbffbdc89f63ab5cf3de3fab48abf2b5909e75
cesar822 <62480657+cesar822@users.noreply.github.com>
2024-10-07 09:36:35 -0300

    Add files via upload

---
+++ code.js
@@ -0,0 +1,9 @@
+document.getElementById('spamton').addEventListener('click', function() {
+    document.getElementById('audio-spamton').volume = 0.1;
+    document.getElementById('audio-spamton').play();
+});
+
+document.getElementById('sonic').addEventListener('click', function() {
+    document.getElementById('audio-sonic').volume = 0.1;
+    document.getElementById('audio-sonic').play();
+});
